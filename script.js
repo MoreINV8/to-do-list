5b53e5290f2f20909254301c8c7b10c8387d6cd1
INV8 <chaiyapat.sr@ku.th>
2023-11-18 21:14:06 +0700

    add fuction to the application

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+const inputField = document.getElementById("input-box");
+const taskField = document.getElementById("tasks");
 
+function createTask() {
+    if (inputField.value === "") {
+        alert("Please enter something...");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputField.value;
+        
+        let span = document.createElement("span");
+        span.innerHTML = "×";
+
+        li.appendChild(span);
+
+        taskField.appendChild(li);
+
+        updateData();
+    }
+    inputField.value = "";
+}
+
+taskField.addEventListener("click", (e) => {
+    if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        updateData();
+    } else if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        updateData();
+    }
+})
+
+function updateData() {
+    localStorage.setItem("data", taskField.innerHTML);
+}
+
+function loadData() {
+    taskField.innerHTML = localStorage.getItem("data");
+}
+
+loadData();
